8d1880f360ea011d67533aabbfc3f8aa4a483796
Daan van Dalfsen <daan.vandalfsen@sudent.rocva.nl>
2021-06-25 00:21:21 +0200

    taken\

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$persoon["voornaam"] = "Daan";
+$persoon["achernaam"] = "van Dalfsen"; 
+$persoon["leeftijd"] = "17";
+$persoon["opleiding"] = "Software Developer";
+
+echo  "Mijn naam is " . $persoon['voornaam'] ." ". $persoon['achternaam'] . "ik ben " . $persoon['leeftijd'] ." ". "jaar oud " . "ik doe de opleiding " . $persoon['opleiding'];
