fe7f2bb303039f115a0ba0699663884896f3deca
Daan van Dalfsen <daan.vandalfsen@sudent.rocva.nl>
2021-06-25 00:09:25 +0200

    taken

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,7 +1,25 @@
 <?php
+ 
+$voornaam['Piet'] = "Piet";
+$voornaam['Muhammad'] = "Muhammad";
+$voornaam['Els'] = "Els";
+ 
+ $lengte['Piet'] = 187;
+ $lengte['Muhammad'] = 170;
+ $lengte['Els'] = 177;
 
-$lengte['Piet'] = 187;
-$lengte['Muhammad'] = 184;
-$lengte['Els'] = 168;
+ $achternaam['Piet'] = "bakker";
+ $achternaam['Muhammad'] = "ali";
+ $achternaam['Els'] = "moolen";
 
-echo"Voornaam". $Voornaam
+ $leeftijt['Piet'] = 16;
+ $leeftijt['Muhammad'] = 18;
+ $leeftijt['Els'] = 17;
+
+ $klas['Piet'] = "0D";
+ $klas['Muhammad'] = "0C";
+ $klas['Els'] = "0D";
+
+ echo "De lengte van" . "$voornaam[Muhammad]" . "$achternaam[Muhammad]" . "is" . "$lengte[Muhammad]" . "centimeter" . "hij is " . "$leeftijt[Muhammad]" . "jaar oud" . "hij zit in de klas" . "$klas[Muhammad]".;
+
+?>
