4758db4ff4704d96c4d2342190ff0822195e5f52
Daan van Dalfsen <daan.vandalfsen@sudent.rocva.nl>
2021-09-07 10:20:57 +0200

    taken

---
+++ 2-Loops/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<body>
+
+<?php  
+$leeftijd = 0;
+
+while($leeftijd <18){
+echo "<li>Ik ben $leeftijd jaar, dus ik mag nog niet stemmen: $leeftijd</li>";
+$leeftijd++;
+
+
+}
+echo  "<li>ik ben 18 jaar oud dus ik heb stemrecht!</li>";
+?>  
+
+</body>
